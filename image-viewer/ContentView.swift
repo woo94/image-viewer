c7ceb0aa5e6f0ec6ada4be27f1af8834dea66eae
woo94 <bestman21c@naver.com>
2024-01-31 12:32:48 +0900

    Initial Commit

---
+++ image-viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  image-viewer
+//
+//  Created by woo94 on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ image-viewer/image_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_viewerApp.swift
+//  image-viewer
+//
+//  Created by woo94 on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct image_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
